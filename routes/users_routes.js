50a6e25efa0d474086ec0a128966cf3611642478
Luthfi Eko Trinowo <luthfii.hakiim@gmail.com>
2020-04-23 06:58:45 +0700

    testing endpoint

--- routes/users_routes.js
+++ routes/users_routes.js
@@ -10,7 +10,7 @@ const results400 = {
 
 const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-router.post('/', function(req, res) {
+router.get('/', function(req, res) {
     res.status(200).send("HOME USER")
 });
 
